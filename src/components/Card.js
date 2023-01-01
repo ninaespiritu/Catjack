a7f2a2a6159f268aaebb558da1948cea185ddb35
Nina Espiritu <ninaangela.espiritu@gmail.com>
2023-01-01 05:41:13 +0000

    Style/design Card component

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,17 +3,6 @@ import "../styles/Card.css";
 export const Card = ({ card, colorCard }) => {
 	// Value of colorCard is set as backgroundImage
 	const colorCardStyle = {
-		margin: "0 3px",
-		width: "100px",
-		height: "150px",
-		display: "flex",
-		flexDirection: "column",
-		alignItems: "center",
-		justifyContent: "center",
-		borderRadius: "20px",
-		backgroundRepeat: "no-repeat",
-		backgroundPosition: "center center",
-		backgroundSize: "contain",
 		backgroundImage: `url(${colorCard})`,
 	};
 
