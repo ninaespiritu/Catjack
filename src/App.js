22c8a7cbfe7b537d426e3b70fc00074628c7d246
Nina Espiritu <ninaangela.espiritu@gmail.com>
2023-01-04 02:14:18 +0000

    Move all game functionality to App component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import "./App.css";
 import Home from "./pages/Home";
 import Catjack from "./pages/Catjack";
@@ -9,10 +9,25 @@ import cardPink from "./assets/cardPink.png";
 import cardPurple from "./assets/cardPurple.png";
 import cardDealer from "./assets/cardDealer.png";
 import cardHide from "./assets/cardHide.png";
+import { fullDeck } from "./utils/Deck";
+import { getCard, getPoints, startGame } from "./utils/Functions";
+import { calculateScore } from "./utils/CalculateScore";
 
 const App = () => {
-	// VARIABLE: Click "Play Now" to set value to true, then render Catjack
+	// Click "Play Now" to set value to true, then render Catjack
 	const [playing, setPlaying] = useState(false);
+
+	// VARIABLES: Game
+	const [gameDeck, setGameDeck] = useState([...fullDeck]);
+	const [gameOver, setGameOver] = useState(false);
+	const [turnOver, setTurnOver] = useState(false);
+	const [result, setResult] = useState("");
+	const [catLives, setCatLives] = useState(3);
+	const [playerCards, setPlayerCards] = useState([]);
+	const [dealerCards, setDealerCards] = useState([]);
+	const [playerScore, setPlayerScore] = useState(0);
+	const [dealerScore, setDealerScore] = useState(0);
+	const dealerTempScore = getPoints(dealerCards[0]);
 
 	// VARIABLEs: Design
 	const [colorCard, setColorCard] = useState(cardGreen);
@@ -24,32 +39,195 @@ const App = () => {
 	const [musicPlaying, setMusicPlaying] = useState(false);
 	const musicRef = useRef(new Audio(musicBackground));
 
+	// Toggle music on/off
 	const musicToggle = () => {
 		if (!musicPlaying) {
 			setMusicPlaying(true);
 			musicRef.current.play();
+			musicRef.current.loop = true;
 		} else if (musicPlaying) {
 			setMusicPlaying(false);
 			musicRef.current.pause();
 		}
 	};
 
+	// Click "Play now" to autoplay background music
 	const musicPlay = () => {
 		if (!musicAutoplay) {
 			setMusicAutoplay(true);
 			setMusicPlaying(true);
 			console.log(musicAutoplay);
 			musicRef.current.play();
+			musicRef.current.loop = true;
 		}
 	};
+
+	// Once page loads, call startGame() to get player's and dealer's cards
+	useEffect(() => {
+		if (playerScore === 0 && dealerScore === 0) {
+			startGame({
+				gameDeck,
+				setGameDeck,
+				playerCards,
+				setPlayerCards,
+				playerScore,
+				setPlayerScore,
+				dealerCards,
+				setDealerCards,
+				dealerScore,
+				setDealerScore,
+			});
+		}
+	}, [
+		gameDeck,
+		setGameDeck,
+		playerCards,
+		setPlayerCards,
+		playerScore,
+		setPlayerScore,
+		dealerCards,
+		setDealerCards,
+		dealerScore,
+		setDealerScore,
+	]);
+
+	// Constantly checks for automatic wins/loses
+	useEffect(() => {
+		if (dealerScore === 21 && dealerCards.length === 2) {
+			setResult("Dealer has Catjack.");
+		} else if (playerScore === 21 && playerCards.length === 2) {
+			setResult("Player has Catjack.");
+		} else if (playerScore === 21 && dealerScore < 21) {
+			setResult("Player scores 21.");
+		} else if (playerScore > 21) {
+			setResult("Player went over 21.");
+		}
+	}, [
+		playerScore,
+		dealerScore,
+		playerCards.length,
+		dealerCards.length,
+		catLives,
+		setCatLives,
+	]);
+
+	// Calculates lives left after an automatic win/lose
+	const calculateResultLives = () => {
+		if (dealerScore === 21 && dealerCards.length === 2) {
+			setTurnOver(true);
+			const loseCatLives = catLives - 1;
+			setCatLives(loseCatLives);
+		} else if (playerScore === 21 && playerCards.length === 2) {
+			setTurnOver(true);
+			const addCatLives = catLives + 1;
+			setCatLives(addCatLives);
+		} else if (playerScore > 21) {
+			setTurnOver(true);
+			const loseCatLives = catLives - 1;
+			setCatLives(loseCatLives);
+		}
+	};
+
+	// If there is an automatic win/lose, run above function
+	if (!turnOver && result !== "") {
+		calculateResultLives();
+	}
+
+	// Calculates winner and lives left after player clicks "Stand"
+	const calculateResult = () => {
+		if (
+			(playerScore === 21 && dealerScore === 21) ||
+			playerScore === dealerScore
+		) {
+			dealerHit() ? dealerHit() : setResult("Both hands are equal.");
+		} else if (dealerScore < playerScore) {
+			dealerHit() ? dealerHit() : setResult("Player scores higher.");
+		} else if (playerScore < dealerScore && dealerScore > 21) {
+			setResult("Dealer went over 21.");
+		} else if (playerScore < dealerScore) {
+			setResult("Dealer scores higher.");
+			const loseCatLives = catLives - 1;
+			setCatLives(loseCatLives);
+		}
+	};
+
+	// If player's turn ends by clicking "Stand", run above function
+	if (turnOver && result === "") {
+		setTimeout(() => {
+			calculateResult();
+		}, 1000);
+	}
+
+	// Player clicks "Hit" to draw new card
+	const buttonHit = (cards, setCards, score, setScore) => {
+		const newCard = getCard({ gameDeck, setGameDeck });
+		const newCards = [...cards, newCard];
+		setCards(newCards);
+		const newScore = score + getPoints(newCard);
+		const finalScore = calculateScore(newCards, newScore);
+		setScore(finalScore);
+	};
+
+	// Player clicks "Stand" to end turn
+	const buttonStand = () => {
+		setTurnOver(true);
+	};
+
+	// Player clicks "Play Again" to start new game
+	const buttonPlayAgain = () => {
+		const refreshDeck = fullDeck;
+		console.log(refreshDeck);
+		setGameDeck([...refreshDeck]);
+		setPlayerCards([]);
+		setDealerCards([]);
+		setPlayerScore(0);
+		setDealerScore(0);
+		setTurnOver(false);
+		setResult("");
+	};
+
+	// Dealer repeatedly calls buttonHit() during its turn
+	const dealerHit = () => {
+		if (dealerScore < 18) {
+			buttonHit(dealerCards, setDealerCards, dealerScore, setDealerScore);
+			return true;
+		} else {
+			return false;
+		}
+	};
+
+	useEffect(() => {
+		if (catLives === 0) {
+			setTimeout(() => {
+				setGameOver(true);
+			}, 1500);
+		}
+	}, [catLives]);
 
 	return (
 		<div className="app">
 			{playing ? (
 				<Catjack
+					setPlaying={setPlaying}
 					colorCard={colorCard}
 					colorCardDealer={colorCardDealer}
 					colorCardHide={colorCardHide}
+					gameOver={gameOver}
+					setGameOver={setGameOver}
+					turnOver={turnOver}
+					result={result}
+					catLives={catLives}
+					setCatLives={setCatLives}
+					playerCards={playerCards}
+					dealerCards={dealerCards}
+					playerScore={playerScore}
+					dealerScore={dealerScore}
+					dealerTempScore={dealerTempScore}
+					setPlayerCards={setPlayerCards}
+					setPlayerScore={setPlayerScore}
+					buttonHit={buttonHit}
+					buttonStand={buttonStand}
+					buttonPlayAgain={buttonPlayAgain}
 				/>
 			) : (
 				<Home setPlaying={setPlaying} musicPlay={musicPlay} />
--- src/pages/Catjack.js
+++ src/pages/Catjack.js
@@ -1,168 +1,28 @@
-import { useEffect, useState } from "react";
 import "../styles/Catjack.css";
 import Card from "../components/Card";
-import { fullDeck } from "../utils/Deck";
-import { getCard, getPoints, startGame } from "../utils/Functions";
-import { calculateScore } from "../utils/CalculateScore";
 
-const Catjack = ({ colorCard, colorCardDealer, colorCardHide }) => {
-	// VARIABLES: Game
-	const [gameDeck, setGameDeck] = useState([...fullDeck]);
-	const [gameOver, setGameOver] = useState(false);
-	const [turnOver, setTurnOver] = useState(false);
-	const [result, setResult] = useState("");
-	const [catLives, setCatLives] = useState(9);
-	const [playerCards, setPlayerCards] = useState([]);
-	const [dealerCards, setDealerCards] = useState([]);
-	const [playerScore, setPlayerScore] = useState(0);
-	const [dealerScore, setDealerScore] = useState(0);
-	const dealerTempScore = getPoints(dealerCards[0]);
-
-	// One page loads, call startGame() to get player's and dealer's cards
-	useEffect(() => {
-		if (playerScore === 0 && dealerScore === 0) {
-			startGame({
-				gameDeck,
-				setGameDeck,
-				playerCards,
-				setPlayerCards,
-				playerScore,
-				setPlayerScore,
-				dealerCards,
-				setDealerCards,
-				dealerScore,
-				setDealerScore,
-			});
-		}
-	}, [
-		gameDeck,
-		setGameDeck,
-		playerCards,
-		setPlayerCards,
-		playerScore,
-		setPlayerScore,
-		dealerCards,
-		setDealerCards,
-		dealerScore,
-		setDealerScore,
-	]);
-
-	// Constantly checks for automatic wins/loses
-	useEffect(() => {
-		if (dealerScore === 21 && dealerCards.length === 2) {
-			setResult("Dealer has Blackjack. You lose.");
-		} else if (playerScore === 21 && playerCards.length === 2) {
-			setResult("You have Blackjack! You win!");
-		} else if (playerScore === 21 && dealerScore < 21) {
-			setResult("You win!");
-		} else if (playerScore > 21) {
-			setResult("You went over 21. You lose.");
-		}
-	}, [
-		playerScore,
-		dealerScore,
-		playerCards.length,
-		dealerCards.length,
-		catLives,
-		setCatLives,
-	]);
-
-	// Calculates lives left after an automatic win/lose
-	const calculateResultLives = () => {
-		if (dealerScore === 21 && dealerCards.length === 2) {
-			setTurnOver(true);
-			const loseCatLives = catLives - 1;
-			setCatLives(loseCatLives);
-		} else if (playerScore === 21 && playerCards.length === 2) {
-			setTurnOver(true);
-			const addCatLives = catLives + 1;
-			setCatLives(addCatLives);
-		} else if (playerScore > 21) {
-			setTurnOver(true);
-			const loseCatLives = catLives - 1;
-			setCatLives(loseCatLives);
-		}
-	};
-
-	// If there is an automatic win/lose, run above function
-	if (!turnOver && result !== "") {
-		calculateResultLives();
-	}
-
-	// Calculates winner and lives left after player clicks "Stand"
-	const calculateResult = () => {
-		if (
-			(playerScore === 21 && dealerScore === 21) ||
-			playerScore === dealerScore
-		) {
-			dealerHit() ? dealerHit() : setResult("It's a tie!");
-		} else if (dealerScore < playerScore) {
-			dealerHit()
-				? dealerHit()
-				: setResult("You have the higher score. You win!");
-		} else if (playerScore < dealerScore && dealerScore > 21) {
-			setResult("Dealer went over 21. You win!");
-		} else if (playerScore < dealerScore) {
-			setResult("Dealer has the higher score. You lose.");
-			const loseCatLives = catLives - 1;
-			setCatLives(loseCatLives);
-		}
-	};
-
-	// If player's turn ends by clicking "Stand", run above function
-	if (turnOver && result === "") {
-		setTimeout(() => {
-			calculateResult();
-		}, 1000);
-	}
-
-	// Player clicks "Hit" to draw new card
-	const buttonHit = (cards, setCards, score, setScore) => {
-		const newCard = getCard({ gameDeck, setGameDeck });
-		const newCards = [...cards, newCard];
-		setCards(newCards);
-		const newScore = score + getPoints(newCard);
-		const finalScore = calculateScore(newCards, newScore);
-		setScore(finalScore);
-	};
-
-	// Player clicks "Stand" to end turn
-	const buttonStand = () => {
-		setTurnOver(true);
-	};
-
-	// Player clicks "Play Again" to start new game
-	const buttonPlayAgain = () => {
-		const refreshDeck = fullDeck;
-		console.log(refreshDeck);
-		setGameDeck([...refreshDeck]);
-		setPlayerCards([]);
-		setDealerCards([]);
-		setPlayerScore(0);
-		setDealerScore(0);
-		setTurnOver(false);
-		setResult("");
-	};
-
-	// Dealer repeatedly calls buttonHit() during its turn
-	const dealerHit = () => {
-		if (dealerScore < 17 || dealerScore < playerScore) {
-			buttonHit(dealerCards, setDealerCards, dealerScore, setDealerScore);
-			return true;
-		} else {
-			return false;
-		}
-	};
-
-	useEffect(() => {
-		if (catLives === 0) {
-			setTimeout(() => {
-				setGameOver(true);
-			}, 2000);
-		}
-	}, [catLives]);
-
-	// ===============================================
+const Catjack = ({
+	setPlaying,
+	colorCard,
+	colorCardDealer,
+	colorCardHide,
+	gameOver,
+	setGameOver,
+	turnOver,
+	result,
+	catLives,
+	setCatLives,
+	playerCards,
+	dealerCards,
+	playerScore,
+	dealerScore,
+	dealerTempScore,
+	setPlayerCards,
+	setPlayerScore,
+	buttonHit,
+	buttonStand,
+	buttonPlayAgain,
+}) => {
 	return (
 		<div className="play">
 			<div>
@@ -175,11 +35,16 @@ const Catjack = ({ colorCard, colorCardDealer, colorCardHide }) => {
 			{gameOver ? (
 				<div>
 					<h2>Game Over</h2>
-					{/* <Link to="/"> */}
-					<button onClick={() => window.location.reload()}>
+					<button
+						onClick={() => {
+							setPlaying(false);
+							setGameOver(false);
+							setCatLives(3);
+							buttonPlayAgain();
+						}}
+					>
 						Back to Home
 					</button>
-					{/* </Link> */}
 				</div>
 			) : (
 				<div>
@@ -207,6 +72,20 @@ const Catjack = ({ colorCard, colorCardDealer, colorCardHide }) => {
 					)}
 
 					<p>{result}</p>
+					<h1>
+						{result === "Player has Catjack." ||
+						result === "Player scores 21." ||
+						result === "Player scores higher." ||
+						result === "Dealer went over 21."
+							? "You win!"
+							: result === "Dealer has Catjack." ||
+							  result === "Player went over 21." ||
+							  result === "Dealer scores higher."
+							? "You lose."
+							: result === "Both hands are equal."
+							? "Tie!"
+							: ""}
+					</h1>
 
 					<h2>Player Score: {playerScore}</h2>
 					<div style={{ display: "flex" }}>
