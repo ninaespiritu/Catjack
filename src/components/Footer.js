eadca77d15e42e92648053ccaa6b4547ea2687cf
Nina Espiritu <ninaangela.espiritu@gmail.com>
2023-01-05 23:58:32 +0000

    Update Footer to toggle open/close on smaller screens

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,7 @@
 import "../styles/Footer.css";
-import { BsVolumeUpFill, BsVolumeMuteFill, BsGithub } from "react-icons/bs";
+import { BsGithub } from "react-icons/bs";
+import { FaPlayCircle, FaPauseCircle } from "react-icons/fa";
+import { useState } from "react";
 
 const Footer = ({
 	musicToggle,
@@ -10,51 +12,67 @@ const Footer = ({
 	cardPink,
 	cardPurple,
 }) => {
+	const [open, setOpen] = useState(false);
+
+	// FUNCTION: Toggle open/close footer menu on smaller devices
+	const handleOpen = () => {
+		setOpen(!open);
+	};
+
 	return (
-		<footer className="footer">
-			<div className="footer-group">
-				<div className="footer-icon">
-					<button
-						className="button-color card-green"
-						onClick={() => setColorCard(cardGreen)}
-					></button>
-					<button
-						className="button-color card-pink"
-						onClick={() => setColorCard(cardPink)}
-					></button>
-					<button
-						className="button-color card-purple"
-						onClick={() => setColorCard(cardPurple)}
-					></button>
+		<footer>
+			<div className={open ? "footer-menu active" : "footer-menu"}>
+				<div className="footer-group">
+					<div className="footer-icon">
+						<button
+							className="button-color card-green"
+							onClick={() => setColorCard(cardGreen)}
+						></button>
+						<button
+							className="button-color card-pink"
+							onClick={() => setColorCard(cardPink)}
+						></button>
+						<button
+							className="button-color card-purple"
+							onClick={() => setColorCard(cardPurple)}
+						></button>
+					</div>
+					<p>
+						colour:{" "}
+						{colorCard === cardGreen
+							? "green"
+							: colorCard === cardPink
+							? "pink"
+							: "purple"}
+					</p>
 				</div>
-				<p>
-					colour:{" "}
-					{colorCard === cardGreen
-						? "green"
-						: colorCard === cardPink
-						? "pink"
-						: "purple"}
-				</p>
-			</div>
 
-			<div className="footer-group" onClick={() => musicToggle()}>
-				<div className="footer-icon react">
-					{musicPlaying ? <BsVolumeUpFill /> : <BsVolumeMuteFill />}
-				</div>
-				<p>music: {musicPlaying ? "on" : "off"}</p>
-			</div>
-
-			<div className="footer-group">
-				<div className="footer-icon react">
-					<BsGithub />
-				</div>
-				<a
-					href="https://github.com/ninaespiritu/Catjack"
-					target="_blank"
-					rel="noreferrer"
+				<div
+					className="footer-group music"
+					onClick={() => musicToggle()}
 				>
-					<p>github</p>
-				</a>
+					<div className="footer-icon react">
+						{musicPlaying ? <FaPlayCircle /> : <FaPauseCircle />}
+					</div>
+					<p>music: {musicPlaying ? "on" : "off"}</p>
+				</div>
+
+				<div className="footer-group">
+					<div className="footer-icon react">
+						<BsGithub />
+					</div>
+					<a
+						href="https://github.com/ninaespiritu/Catjack"
+						target="_blank"
+						rel="noreferrer"
+					>
+						<p>github</p>
+					</a>
+				</div>
+			</div>
+
+			<div className="hamburger" onClick={handleOpen}>
+				<button>{open ? "Close Menu" : "Open Menu"}</button>
 			</div>
 		</footer>
 	);
